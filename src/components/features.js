de042aea36366c2d51a2eff40f2c720875eb7d87
Jaasim <479jaasim@gmail.com>
2025-07-26 18:12:23 +0530

    Image container bg-color changed

--- src/components/features.js
+++ src/components/features.js
@@ -34,7 +34,9 @@ export default function Features({classlist}){
                     {featuresData.map((item,index) => {
                         return(
                         <div className="relative overflow-hidden bg-white dark:bg-slate-900 rounded-md shadow dark:shadow-gray-800" key={index}>
-                            <div className="p-6 pb-0 relative overflow-hidden after:content-[''] after:absolute after:inset-0 after:mx-auto after:w-72 after:h-72 after:bg-gradient-to-tl after:to-amber-400 after:from-fuchsia-600 after:blur-[80px] after:rounded-full">
+                            <div className="p-6 pb-0 relative overflow-hidden after:content-[''] after:absolute after:inset-0 after:mx-auto after:w-72 after:h-72 
+                            after:bg-gradient-to-tl after:from-blue-500 after:to-sky-400
+ after:blur-[80px] after:rounded-full">
                                 <img src={item.image} className="relative rounded-t-md shadow-md dark:shadow-slate-700 z-1" alt=""/>
                             </div>
 
